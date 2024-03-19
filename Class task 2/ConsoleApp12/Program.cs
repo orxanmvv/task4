4718376cdacaa9099fb7dead7cf71672cc2c7668
orxanmvv <m.orxan.m123@gmail.com>
2024-03-20 03:09:21 +0400

    4cu commit

---
+++ Class task 2/ConsoleApp12/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("Sony", "bizden alin razi qalin", 5, 999.0, 16, 1000);
+            notebook.ShowFullData();
+            notebook.Sale();
+        }
+    }
+
+    public class Product
+    {
+        public string Name;
+        public string Description;
+        public int Count;
+        public bool IsStock;
+        public double Price;
+
+        public Product(string name, string description, int count, double price)
+        {
+            Name = name;
+            Description = description;
+            Count = count;
+            Price = price;
+            IsStock = count > 0;
+        }
+
+        public void Sale()
+        {
+            if (Count > 0)
+            {
+                Count--;
+                IsStock = Count > 0;
+                Console.WriteLine("satilib.");
+            }
+            else
+            {
+                Console.WriteLine("stokda yoxdur!");
+            }
+        }
+    }
+
+    public class Notebook : Product
+    {
+        public byte RAM;
+        public int Storage;
+
+        public Notebook(string name, string description, int count, double price, byte ram, int storage)
+            : base(name, description, count, price)
+        {
+            RAM = ram;
+            Storage = storage;
+        }
+
+        public string GetFullInfo()
+        {
+            return $"Name: {Name}, Description: {Description}, Count: {Count}, Price: {Price}, RAM: {RAM}, Storage: {Storage}";
+        }
+
+        public void ShowFullData()
+        {
+            if (IsStock)
+            {
+                Console.WriteLine(GetFullInfo());
+            }
+            else
+            {
+                Console.WriteLine("Məhsul yoxdur!");
+            }
+        }
+    }
+}
+
---
+++ Class task1/ConsoleApp6/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car("Mercedes", "CLS", 10000, 4);
+            Motorcycle motorcycle = new Motorcycle("Kawasaki", "Ninja", 5000, 2);
+
+            Console.WriteLine(car.ShowFullInfo());
+            Console.WriteLine(motorcycle.ShowFullInfo());
+
+        }
+        public class Vehicle
+        {
+            public string Brand;
+            public string Model;
+            public double Mile;
+
+            public Vehicle(string brand, string model, double mile)
+            {
+                if (string.IsNullOrEmpty(brand) || string.IsNullOrEmpty(model))
+                {
+                    throw new ArgumentException("Marka ve Model belirtilmelidir.");
+                }
+
+                Brand = brand;
+                Model = model;
+                Mile = mile;
+            }
+        }
+
+        public class Car : Vehicle
+        {
+            public int DoorsNum;
+
+            public Car(string brand, string model, double mile, int doorsNum)
+                : base(brand, model, mile)
+            {
+                DoorsNum = doorsNum;
+            }
+
+            public string ShowFullInfo()
+            {
+                return $"Marka: {Brand}, Model: {Model}, Km: {Mile}, Qapi Sayısı: {DoorsNum}";
+            }
+        }
+
+        public class Motorcycle : Vehicle
+        {
+            public int WheelNum;
+
+            public Motorcycle(string brand, string model, double mile, int wheelNum)
+                : base(brand, model, mile)
+            {
+                WheelNum = wheelNum;
+            }
+
+            public string ShowFullInfo()
+            {
+                return $"Marka: {Brand}, Model: {Model}, Km: {Mile}, Teker Sayısı: {WheelNum}";
+            }
+        }
+
+
+
+
+
+
+    }
+   
+}
